9c81fae9a4503a6ce2eeeccd457f03354a98bbab
Zayne Sprague <zayne_sprague@yahoo.com>
2023-01-31 21:22:50 -0600

    config runner changes

--- config_runner/run.py
+++ config_runner/run.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import subprocess
 from pathlib import Path
 from typing import List, Tuple
@@ -12,12 +13,27 @@ TMP_FOLDER = ROOT_FOLDER / 'tmp'
 
 def run(
         configs: List[Path],
+        pid_file_tracker: Path = None,
+        gpu: str = None,
+        prefix_name: str = None
 ):
 
     container_names = []
     names_to_config = {}
 
     for config in tqdm(configs, total=len(configs), desc='Starting containers'):
+        if gpu or prefix_name:
+            c = {}
+            with config.open('r') as f:
+                c = json.load(f)
+            if gpu:
+                c['device'] = gpu
+            if prefix_name:
+                c['run_name'] = f"{prefix_name}/{c['run_name'].split('/')[-1]}"
+
+            with config.open('w') as f:
+                json.dump(c, f)
+
         container_name = str(uuid4()).replace('-', '')
         subprocess.run([str(ROOT_FOLDER / 'run_config.sh'), container_name, f'/home/rosdev/social_gym/config_runner/configs/{config}'], stdout=subprocess.PIPE)
 
@@ -38,6 +54,11 @@ def run(
     print()
     print('Kill all these containers')
     print("; ".join([f"docker kill {x}" for x in container_names]))
+
+    if pid_file_tracker:
+        pid_file_tracker.parent.mkdir(exist_ok=True, parents=True)
+        with pid_file_tracker.open('w') as f:
+            json.dump(names_to_config, f)
 
     status = {k: False for k in container_names}
 
@@ -76,16 +97,24 @@ if __name__ == "__main__":
 
     parser.add_argument('--folder', '-f', type=str, help='Path to a directory that has a batch of json configs to run inside the {PROJECT_ROOT}/config_runner/configs folder.')
     parser.add_argument('--config', '-c', type=str, default=[], nargs='+', help='Individual config files to run inside the {PROJECT_ROOT}/config_runner/configs folder. Can be separated by spaces.')
+    parser.add_argument('--pid_tracking_file', '-p', type=str, help='File that keeps track of the docker names and config files mapping.')
+
+    parser.add_argument('--gpu', '-g', type=str)
+    parser.add_argument('--name', '-n', type=str, help='File that keeps track of the docker names and config files mapping.')
 
     args = parser.parse_args()
 
     folder = args.folder
     configs = [x for x in args.config]
+    pid_file = Path(args.pid_tracking_file) if args.pid_tracking_file else None
+
+    gpu = args.gpu
+    name = args.name
 
     if folder is not None:
         configs.extend([f'{folder}/{x.name}' for x in list((CONFIG_FOLDER / folder).glob("*.json"))])
 
-    run(configs)
+    run(configs, pid_file_tracker=pid_file, gpu=gpu, name=name)
 
 
 
