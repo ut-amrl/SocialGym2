3dd2df12404d5dbba89a5ef97b46f8df6d819892
Rohan Chandra <rchandra@utexas.edu>
2023-03-17 17:41:40 -0500

    modified build file for podman

--- config_runner/build.sh
+++ config_runner/build.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-docker build -t social_gym_config_runner:1.0 -f ./config_runner/Dockerfile .
+
+if command -v podman &> /dev/null
+then
+    # Podman is installed
+    docker build --format docker -t social_gym_config_runner:1.0 -f ./config_runner/Dockerfile .
+else
+    if command -v docker &> /dev/null
+    then
+        # Docker is installed
+        docker build -t social_gym_config_runner:1.0 -f ./config_runner/Dockerfile .
+    else
+        # Neither Podman nor Docker is installed
+        echo "Neither Podman nor Docker is installed."
+        exit 1
+    fi
+fi
